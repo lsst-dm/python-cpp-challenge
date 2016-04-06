ffea6aa1c3b7ff639dbf16ab761bf6606d080558
Pim Schellart <P.Schellart@princeton.edu>
2016-04-06 11:36:11 -0400

    Add containers module.
    Also had to fix cross-module types.
    Now build c++ source into separate library (dylib on osx),
    this fixes symbol visibility issues.

--- python2-pybind11/setup.py
+++ python2-pybind11/setup.py
@@ -1,63 +1,50 @@
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 import os, sys
+
+from distutils.core import setup, Extension
+from distutils import sysconfig
+
+cpp_args = ['-std=c++14', '-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+if sys.platform == 'darwin':
+    vars = sysconfig.get_config_vars()
+    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
 ext_modules = [
     Extension(
+	'libbasics',
+        ['src/basics.cpp'],
+        include_dirs=['include'],
+	language='c++',
+	extra_compile_args = cpp_args,
+    ),
+    Extension(
+	'libcontainers',
+        ['src/containers.cpp'],
+        include_dirs=['include'],
+	language='c++',
+	extra_compile_args = cpp_args,
+    ),
+    Extension(
         'challenge.basics',
-        ['challenge/basics.cpp', '../src/basics.cpp'],
-        include_dirs=['pybind11/include', '../include'],
+        ['challenge/basics.cpp'],
+        include_dirs=['pybind11/include', 'include'],
         language='c++',
+        library_dirs=['.'],
+        libraries=['basics'],
+	extra_compile_args = cpp_args,
+#	extra_link_args = ['-v'],
+    ),
+    Extension(
+        'challenge.containers',
+        ['challenge/containers.cpp'],
+        include_dirs=['pybind11/include', 'include'],
+        language='c++',
+        library_dirs=['.'],
+        libraries=['basics','containers'],
+	extra_compile_args = cpp_args,
+#	extra_link_args = ['-v'],
     ),
 ]
-
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    fd, fname = tempfile.mkstemp('.cpp', 'main', text=True)
-    with os.fdopen(fd, 'w') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-    try:
-        compiler.compile([fname], extra_postargs=[flagname])
-    except setuptools.distutils.errors.CompileError:
-        return False
-    return True
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
 
 setup(
     name='challenge',
@@ -65,9 +52,5 @@ setup(
     author='Pim Schellart',
     author_email='P.Schellart@princeton.edu',
     description='Solution to the Python C++ bindings challenge with pybind11.',
-    long_description='',
     ext_modules=ext_modules,
-    install_requires=['pybind11'],
-    cmdclass={'build_ext': BuildExt},
-    zip_safe=False,
 )
