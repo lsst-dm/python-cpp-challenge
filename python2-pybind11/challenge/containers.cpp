fe4f0857475910db8df6ed33852e447a7a01106c
Pim Schellart <P.Schellart@princeton.edu>
2016-04-11 17:11:30 -0400

    Support assignment and conversion to lists and dicts.

--- python2-pybind11/challenge/containers.cpp
+++ python2-pybind11/challenge/containers.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "basics.hpp"
 #include "containers.hpp"
@@ -15,7 +16,11 @@ PYBIND11_PLUGIN(containers) {
     py::class_<containers::DoodadSet>(m, "DoodadSet")
         .def(py::init<>())
         .def("__len__", &containers::DoodadSet::size)
-        .def("add", [](containers::DoodadSet &ds, std::shared_ptr<basics::Doodad> &d) { ds.add(d); });
+        .def("add", [](containers::DoodadSet &ds, std::shared_ptr<basics::Doodad> &d) { ds.add(d); })
+        .def("add", [](containers::DoodadSet &ds, std::pair<std::string, int> p) { ds.add(basics::WhatsIt{p.first, p.second}) ; })
+        .def("as_dict", &containers::DoodadSet::as_map)
+        .def("as_list", &containers::DoodadSet::as_vector)
+        .def("assign", &containers::DoodadSet::assign);
 
     return m.ptr();
 }
