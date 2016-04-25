8e060b51d057dd8d9eff474bd3e818068fb92e65
Pim Schellart <P.Schellart@princeton.edu>
2016-04-25 16:14:18 -0400

    Add class attribute for Item

--- python2-pybind11/challenge/containers.cpp
+++ python2-pybind11/challenge/containers.cpp
@@ -32,8 +32,9 @@ private:
 PYBIND11_PLUGIN(containers) {
     py::module m("containers", "wrapped C++ containers module");
 
-    py::class_<containers::DoodadSet>(m, "DoodadSet")
-        .def(py::init<>())
+    py::class_<containers::DoodadSet> c(m, "DoodadSet");
+
+    c.def(py::init<>())
         .def("__len__", &containers::DoodadSet::size)
         .def("add", (void (containers::DoodadSet::*)(std::shared_ptr<basics::Doodad>)) &containers::DoodadSet::add)
         .def("add", [](containers::DoodadSet &ds, std::pair<std::string, int> p) { ds.add(basics::WhatsIt{p.first, p.second}) ; })
@@ -41,6 +42,8 @@ PYBIND11_PLUGIN(containers) {
         .def("as_dict", &containers::DoodadSet::as_map)
         .def("as_list", &containers::DoodadSet::as_vector)
         .def("assign", &containers::DoodadSet::assign);
+
+    c.attr("Item") = py::module::import("challenge.basics").attr("Doodad");
 
     py::class_<containers::DoodadSetIterator>(m, "DoodadSetIterator")
         .def("__iter__", [](containers::DoodadSetIterator &it) -> containers::DoodadSetIterator& { return it; })
