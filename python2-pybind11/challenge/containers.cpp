8b49116d2e896960f901a01a3af7e95ef2f94499
Jim Bosch <jbosch@astro.princeton.edu>
2016-04-26 17:24:50 -0400

    Switch to using pybind11::make_iterator instead of a custom class.

--- python2-pybind11/challenge/containers.cpp
+++ python2-pybind11/challenge/containers.cpp
@@ -8,27 +8,6 @@ namespace py = pybind11;
 
 PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>);
 
-namespace containers {
-
-class DoodadSetIterator {
-public:
-    DoodadSetIterator(DoodadSet::iterator b, DoodadSet::iterator e) : it{b}, it_end{e} {};
-
-    std::shared_ptr<basics::Doodad> next() {
-        if (it == it_end) {
-            throw py::stop_iteration();
-        } else {
-            return *it++;
-        }
-    };
-
-private:
-    DoodadSet::iterator it;
-    DoodadSet::iterator it_end;
-};
-
-} // namespace containers
-
 PYBIND11_PLUGIN(containers) {
     py::module m("containers", "wrapped C++ containers module");
 
@@ -38,16 +17,13 @@ PYBIND11_PLUGIN(containers) {
         .def("__len__", &containers::DoodadSet::size)
         .def("add", (void (containers::DoodadSet::*)(std::shared_ptr<basics::Doodad>)) &containers::DoodadSet::add)
         .def("add", [](containers::DoodadSet &ds, std::pair<std::string, int> p) { ds.add(basics::WhatsIt{p.first, p.second}) ; })
-        .def("__iter__", [](containers::DoodadSet &ds) { return containers::DoodadSetIterator{ds.begin(), ds.end()}; }, py::keep_alive<0,1>())
+        .def("__iter__", [](containers::DoodadSet &ds) { return py::make_iterator(ds.begin(), ds.end()); },
+             py::keep_alive<0, 1>())
         .def("as_dict", &containers::DoodadSet::as_map)
         .def("as_list", &containers::DoodadSet::as_vector)
         .def("assign", &containers::DoodadSet::assign);
 
     c.attr("Item") = py::module::import("challenge.basics").attr("Doodad");
-
-    py::class_<containers::DoodadSetIterator>(m, "DoodadSetIterator")
-        .def("__iter__", [](containers::DoodadSetIterator &it) -> containers::DoodadSetIterator& { return it; })
-        .def("__next__", &containers::DoodadSetIterator::next);
 
     return m.ptr();
 }
